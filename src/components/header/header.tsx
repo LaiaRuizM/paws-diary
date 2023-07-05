255455b7a9a53b5814117ea441509ef4bd96c77d
LaiaRuizM <laia.ruizm@hotmail.com>
2023-07-05 13:14:29 +0200

    chore: merge branch with master

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,42 @@
-export const Header =()=>{
-    return(
-        <h1>Bienvenido</h1>
-    )
-}
+import { Link } from "react-router-dom";
+import logoAmzn from "../images/removebckgr.png";
+import "../styles/layouts/Header.scss";
+export const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <Link to={"/"}>
+          <img
+            className="header__img"
+            src={logoAmzn}
+            alt="Amazon's logo"
+            title="Amazon's logo"
+          />
+        </Link>
+      </header>
+      <nav className="nav">
+        <ul className="nav__listsHeader">
+          <li>
+            <Link className="nav__link" to={"/products"}>
+              <i class="fa-solid fa-list"></i> Products
+            </Link>
+          </li>
+          <li>
+            <Link className="nav__link" to={"/loginAccount"}>
+              <i class="fa-regular fa-user"></i> Login account
+            </Link>
+          </li>
+          <li>
+            <Link className="nav__link" to={"/shoppingCart"}>
+              <i class="fa-solid fa-cart-shopping"></i> Shopping cart
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <p className="header__text">
+        Discover amazing deals, unbeatable prices and exclusive offers on
+        Amazon!
+      </p>
+    </>
+  );
+};
