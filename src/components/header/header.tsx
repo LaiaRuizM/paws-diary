1ebc014b644aff5b88a10a847caee57bf88198d2
LaiaRuizM <laia.ruizm@hotmail.com>
2023-07-05 13:32:35 +0200

    fix React Routes in index.tsx and install dependencies to package.json

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,34 +1,33 @@
+// import logoPaws from "../public/PawsDiaryLogo4.png";
 import { Link } from "react-router-dom";
-import logoAmzn from "../images/removebckgr.png";
-import "../styles/layouts/Header.scss";
 export const Header = () => {
   return (
     <>
       <header className="header">
         <Link to={"/"}>
-          <img
+          {/* <img
             className="header__img"
-            src={logoAmzn}
+            // src={logoPaws}
             alt="Amazon's logo"
             title="Amazon's logo"
-          />
+          /> */}
         </Link>
       </header>
       <nav className="nav">
         <ul className="nav__listsHeader">
           <li>
             <Link className="nav__link" to={"/products"}>
-              <i class="fa-solid fa-list"></i> Products
+              <i className="fa-solid fa-list"></i> Products
             </Link>
           </li>
           <li>
             <Link className="nav__link" to={"/loginAccount"}>
-              <i class="fa-regular fa-user"></i> Login account
+              <i className="fa-regular fa-user"></i> Login account
             </Link>
           </li>
           <li>
             <Link className="nav__link" to={"/shoppingCart"}>
-              <i class="fa-solid fa-cart-shopping"></i> Shopping cart
+              <i className="fa-solid fa-cart-shopping"></i> Shopping cart
             </Link>
           </li>
         </ul>
