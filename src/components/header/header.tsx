d5f0deb73acf3fc45b3191a538ab4853c1041848
Isabel Collado Herrera <icolladoherrera@gmail.com>
2023-08-24 10:51:05 +0200

    "Feat add  header logo"

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,11 @@
-export const Header =()=>{
-    return(
-        <h1>Bienvenido</h1>
-    )
-}
+//import { Logo } from '../../images/paws-diary-logo-4';
+
+export const Header = () => {
+  return (
+    <>
+      <h1>Paws Diary</h1>
+      <img src="../images/paws-diary-logo-4.png" alt="logo" />
+      <p>hola</p>
+    </>
+  );
+};
